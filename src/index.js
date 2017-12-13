ac7a82df2d09d70a6ac7f11f5ab8eec54ad8359d
github username <dylankershaw@me.com>
2017-12-13 16:30:50 -0500

    index.js and index reducer setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import promise from "redux-promise";
+import ReactDOM from "react-dom";
+import React from "react";
 
-ReactDOM.render(<div />, document.getElementById("root"));
+import "./index.css";
+import reducers from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          {/* Switch renders only the first route that matches */}
+          <Route path="/" component={Login} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
