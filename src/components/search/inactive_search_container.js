8f030dc6df57605610d029768d0416baf293cbd6
github username <dylankershaw@me.com>
2017-12-29 20:49:49 -0500

    root changes background image every 5 seconds

--- src/components/search/inactive_search_container.js
+++ src/components/search/inactive_search_container.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import SearchNavbar from "../navigation/search_navbar";
 
-const InactiveSearchContainer = () => {
-  return (
-    <div
-      style={{
-        backgroundImage:
-          "url(https://storage.googleapis.com/auto-stock-189103.appspot.com/1514594277.jpeg)",
-        backgroundSize: "cover",
-        height: "1000px"
-      }}
-    >
-      <SearchNavbar textColor="white" />
-    </div>
-  );
-};
+class InactiveSearchContainer extends Component {
+  images = ["1514594277.jpeg", "1514597615.jpeg", "1514597663.jpeg"];
+  url = "https://storage.googleapis.com/auto-stock-189103.appspot.com/";
+
+  constructor(props) {
+    super(props);
+
+    this.state = { imageUrl: this.url + this.images[0] };
+  }
+
+  componentDidMount() {
+    let currentImageIndex = 0;
+
+    // cycles through images and changes background every five seconds
+    setInterval(() => {
+      currentImageIndex =
+        currentImageIndex === this.images.length - 1
+          ? 0
+          : currentImageIndex + 1;
+
+      this.setState({ imageUrl: this.url + this.images[currentImageIndex] });
+    }, 5000);
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          backgroundImage: `url(${this.state.imageUrl})`,
+          backgroundSize: "cover",
+          height: "1000px"
+        }}
+      >
+        <SearchNavbar textColor="white" />
+      </div>
+    );
+  }
+}
 
 export default InactiveSearchContainer;
