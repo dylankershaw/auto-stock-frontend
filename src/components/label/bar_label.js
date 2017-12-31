3e61c8235920288986a5719087821e4a0dc25f82
github username <dylankershaw@me.com>
2017-12-31 12:14:35 -0500

    BarLabels animate quickly from 0

--- src/components/label/bar_label.js
+++ src/components/label/bar_label.js
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Progress } from "semantic-ui-react";
 
-const BarLabel = ({ score, name }) => {
-  // Math.round(score * 100);
-  return (
-    <div>
-      <Progress
-        label={name ? name : null}
-        as={name ? Link : null}
-        to={`/search/${name}`}
-        value={Math.round(score * 100)}
-        progress="percent"
-        precision={0}
-        total="100"
-        color="green"
-      />
-    </div>
-  );
-};
+class BarLabel extends Component {
+  barScore = 0;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      score: 0
+    };
+  }
+
+  // triggers bar animation by changing score from 0 after 200ms
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ score: this.props.score });
+    }, 200);
+  }
+
+  render() {
+    return (
+      <div>
+        <Progress
+          label={this.props.name ? this.props.name : null}
+          as={this.props.name ? Link : null}
+          to={`/search/${this.props.name}`}
+          value={Math.round(this.state.score * 100)}
+          progress="percent"
+          precision={0}
+          total="100"
+          color="green"
+        />
+      </div>
+    );
+  }
+}
 
 export default BarLabel;
